c4b402d3aa99af48a715d4e51d2547fa11826fc4
navin-nawshin <navinmdnawshin@gmail.com>
2024-06-05 22:28:44 +0600

    new commit

---
+++ BasicCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;   //statement
+class Program{  //class
+
+    public static void Main(String[] args){ //main method
+        Console.WriteLine("My name is Navin.");    //statement: specific line perform
+        for(int i=0;i<20;i++){
+            Console.Write("Operator: ");
+            char op = Console.ReadLine()[0];
+
+            float a,b;
+            Console.Write("1st num:");
+            a = Convert.ToSingle(Console.ReadLine());
+            Console.Write("2nd num:");
+            b = Convert.ToSingle(Console.ReadLine());
+            
+            switch(op){
+                    case '+':
+                    Console.WriteLine("Result: "+(a+b));
+                    break;
+                    case '-':
+                    Console.WriteLine("Result: "+(a-b));
+                    break;
+                    case '*':
+                    Console.WriteLine("Result: "+(a*b));
+                    break;
+                    case '/':
+                    if(b!=0){
+                    Console.WriteLine("Result: "+(a/b));
+                    }else{
+                        System.Console.WriteLine("Can't be divided");
+                    }break;
+
+                    default:
+                    System.Console.WriteLine("Invalid operator! exiting...");
+                    Environment.Exit(0);
+                    break;
+                }
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+public class Program{
+    public class LinkedList{
+        public class Node{
+        public Node? next;
+        public object? data;
+    }private Node? root;
+
+    public Node First{get{return root;}}
+    public Node? Last{
+        get{
+            Node curNode = root;
+            if(curNode == null){
+                return null;
+            }
+            while(curNode.next != null){
+                curNode = curNode.next;
+            }return curNode;
+        }
+    }
+        public void Append(object value){
+            Node node = new Node{data = value};
+            if(root==null){
+                root = node;
+            }else{
+                Last.next = node;
+            }
+        }
+        public void Delete(Node node){
+            if(root==node){
+                root = node.next;
+                node.next = null;
+            }else{
+                Node current = root;
+                while(current.next != null){
+                    if(current.next == node){
+                        current.next = node.next;
+                        node.next = null;
+                        break;
+                    }current = current.next;
+                }
+            }
+        }
+    }
+    public static void Main(String[] args){}
+}
