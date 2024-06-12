b9c5a8226e1f534ebb3f92e71e98425b99fafde8
navin-nawshin <navinmdnawshin@gmail.com>
2024-06-12 22:54:42 +0600

    Interface implement

---
+++ Interface/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program : IExample,IExample2{
+    public static void Main(String[] args){}
+
+    public void Example(){
+        System.Console.WriteLine("Hi");
+    }
+
+    public int Example2(){
+        return 1;
+    }
+
+    public string Example3(string s1, string s2){
+        return s1+" "+s2;
+    }
+}
+interface IExample{
+        void Example();
+        int Example2();
+        string Example3(string s1, string s2);
+    }
+interface IExample2{}
