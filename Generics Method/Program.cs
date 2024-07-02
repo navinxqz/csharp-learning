1449c5bdd9976df6b9606581a8336f7cb7988057
navin-nawshin <navinmdnawshin@gmail.com>
2024-07-03 01:14:36 +0600

    generics

---
+++ Generics Method/Program.cs
@@ -0,0 +1,15 @@
+﻿class Generics<T>{
+    public static void Print<T>(T obj){
+        System.Console.WriteLine(obj.ToString());
+    }public static void Print<T>(T obj, T obj2){
+        System.Console.WriteLine(obj.ToString() + obj2.ToString());
+    }
+}class Program{
+    static void Main(string[] args){
+        Generics<string>.Print("\nHello World");
+        Generics<double>.Print(9830.023);
+        Generics<bool>.Print(false);
+
+        Generics<string>.Print("Hello World"," I'm Navin");
+    }
+}
