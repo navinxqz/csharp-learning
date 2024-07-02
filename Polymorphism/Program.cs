d5ef1eae5335e2aeecd2bd6be620ff574fcb3e84
navin-nawshin <navinmdnawshin@gmail.com>
2024-07-03 00:41:02 +0600

    virtual&override

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,16 @@
+﻿class Base{
+    public virtual void Run(){
+    System.Console.WriteLine("\tOutput from Base class");
+    }
+}class Child : Base{
+    public override void Run(){
+        System.Console.WriteLine("\tOutput from Child class");
+    }
+}class Program{
+    public static void Main(string[] args){
+        Base obj = new Child();
+        //base method of the same name takes precedence, only
+        //if virtual and override keyword not implemented
+        obj.Run();
+    }
+}
