b585da35ac4ce60cd3949d6d094a40337079a8f8
navin-nawshin <navinmdnawshin@gmail.com>
2024-07-03 00:04:02 +0600

    recursion

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program{
+    static int Fib(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }return Fib(n-1) +Fib(n-2);     //recursion method
+    }
+    public static void Main(string[] args){
+        Console.Write("Input the value: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Fib value: {0}",Fib(n));
+    }
+}
