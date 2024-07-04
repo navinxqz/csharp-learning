86865b0dccdf69bbb57077a7f42ae80eb7844fed
navin-nawshin <navinmdnawshin@gmail.com>
2024-07-04 14:06:31 +0600

    readonly

---
+++ readonly/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program{
+    public struct Stats{
+        public int Atkpower{get;set;}
+        public int Defence{get;set;}
+        public readonly override string ToString(){
+            return $"Atk power: {Atkpower} & Defence: {Defence}\n";
+        }
+    }
+    public static void Main(string[] args){
+        Stats s = new Stats{
+            Atkpower = 600,
+            Defence = 69
+        };
+        Console.WriteLine(s);
+    }
+}
