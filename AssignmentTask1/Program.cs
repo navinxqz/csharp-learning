e3a97988cd45d62e5d07901b528543d40e325bf9
navin-nawshin <navinmdnawshin@gmail.com>
2024-07-16 08:35:18 +0600

    class init

--- AssignmentTask1/Program.cs
+++ AssignmentTask1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace UnversityLib{
+    public class Date{
+        public int Year{ get; set; }
+        public int Month{ get; set; }
+        public int Day{ get; set; }
+        public Date(int day, int month, int year){
+            this.Year = year;
+            this.Month = month;
+            this.Day = day;
+        }
+    }public abstract class Book{
+        public string ISBN{ get; set; }
+        public string Title{ get; set; }
+        public string Author{get;set; }
+        public Date PubDate{get;set; }
+        public Book(string ISBN, string Title, string Author,Date pubDate){
+            this.ISBN = ISBN;
+            this.Title = Title;
+            this.Author = Author;
+            this.PubDate = pubDate;
+        }
+    }
+}
