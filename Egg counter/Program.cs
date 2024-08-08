b1a162f2dde4e642743331b39b5dd8766e8054c3
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-08 14:21:12 +0600

    fab

--- Egg counter/Program.cs
+++ Egg counter/Program.cs
@@ -1,14 +1,12 @@
 ﻿class Program{
     public static int HavingEggs(int n){
-        if(n == 1){ return 1;}
-
-        return 2 * HavingEggs(n-1);
+        if(n==1){ return 1;}
+        return 2* HavingEggs(n-1);
     }
-
     public static void Main(string[] args){
-        Console.Write("Num of students in the classroom: ");
+        Console.Write("Total students in the classroom: ");
         int n = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine(HavingEggs(n));
+        Console.WriteLine($"The last student has --> {HavingEggs(n)} eggs");
     }
 }
