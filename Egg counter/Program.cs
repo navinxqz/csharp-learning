e2c9f40dd96543f4587d5d8da3cf447387124275
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-08 14:16:41 +0600

    recursive

--- Egg counter/Program.cs
+++ Egg counter/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿class Program{
+    public static int HavingEggs(int n){
+        if(n == 1){ return 1;}
+
+        return 2 * HavingEggs(n-1);
+    }
+
+    public static void Main(string[] args){
+        Console.Write("Num of students in the classroom: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(HavingEggs(n));
+    }
+}
