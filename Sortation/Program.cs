a2c64b3e8d6b119ac4fe6e9b62c48fe0775d0327
navin-nawshin <navinmdnawshin@gmail.com>
2024-10-23 21:22:42 +0600

    Sort()

---
+++ Sortation/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] p = ["B42", "A11", "B12", "M14", "A13", "M13", "C12"];
+System.Console.WriteLine("Sorted...");
+Array.Sort(p);
+
+foreach (string s in p)
+{
+    System.Console.WriteLine("--> " +s);    //forward
+}
