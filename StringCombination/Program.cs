f8a83bd49021cf9ebae46bc63fbe9d1dd4425dd0
navin-nawshin <navinmdnawshin@gmail.com>
2024-10-23 21:13:44 +0600

    combining string array values as strings and as integers

---
+++ StringCombination/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0m;
+string message = string.Empty;
+
+foreach (string value in values)
+{
+    decimal number;
+    if (decimal.TryParse(value, out number))
+    {
+        total += number;
+    }
+    else
+    {
+        message += value; //+ Environment.NewLine for new line;
+    }
+}
+System.Console.WriteLine("Message: " + message);
+System.Console.WriteLine("Total: " + total);
