bcdceddb99a9b742b6a3b8365d6f86451766f8c6
navin-nawshin <navinmdnawshin@gmail.com>
2024-10-23 23:00:47 +0600

    padding space understanding

--- PaddingAlignment/Program.cs
+++ PaddingAlignment/Program.cs
@@ -1,9 +1,11 @@
 ﻿string id = "E-616";
 string name = "Wanda Maximoff";
 string payment = "$3,000.00";
+string notes = "Monthly payment for contract #E-616.";
 
 var lineFormat = id.PadRight(15);
 lineFormat += name.PadRight(20);
 lineFormat += payment.PadLeft(10);
 
 System.Console.WriteLine(lineFormat);
+System.Console.WriteLine(notes.PadLeft(12));    //one space added to the left
