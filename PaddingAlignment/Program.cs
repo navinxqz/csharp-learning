c911366c3d7be8063b61df1aef77f1efea203572
navin-nawshin <navinmdnawshin@gmail.com>
2024-10-23 22:57:44 +0600

    padding and alignment

---
+++ PaddingAlignment/Program.cs
@@ -0,0 +1,9 @@
+﻿string id = "E-616";
+string name = "Wanda Maximoff";
+string payment = "$3,000.00";
+
+var lineFormat = id.PadRight(15);
+lineFormat += name.PadRight(20);
+lineFormat += payment.PadLeft(10);
+
+System.Console.WriteLine(lineFormat);
