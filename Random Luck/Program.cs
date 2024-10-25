cd38dcc8d8f4bae8fc49bac8f174f28c26dc87ce
navin-nawshin <navinmdnawshin@gmail.com>
2024-10-25 23:41:33 +0600

    fortune teller

---
+++ Random Luck/Program.cs
@@ -0,0 +1,18 @@
+﻿Random rand = new Random();
+int luck = rand.Next(1, 101);
+
+string[] txt = {"You have much to ","Today is the day to ", "Whatever work you do ", "This is an ideal time to "};
+string[] good = {"look forward to.", "be creative.", "will be successful.", "make a change."};
+string[] bad = {"be cautious.", "avoid conflict.", "be patient.", "be careful."};
+string[] neutral = {"be productive.", "enjoy time with friends and family.", "be realistic.", "rethink your goals."};
+
+Fortune();
+
+void Fortune(){
+    System.Console.WriteLine("Your luck for today is: ");
+    string[] fortune = (luck > 70) ? good : (luck < 30) ? bad : neutral;
+
+    for(int i = 0; i < 4; i++){
+        System.Console.WriteLine(txt[i] + fortune[i]);
+    }
+}
