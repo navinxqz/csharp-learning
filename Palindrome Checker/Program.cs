80fa7d5154a32fa9d4c6dadeb8e34f31b5283c0d
navin-nawshin <navinmdnawshin@gmail.com>
2024-10-25 23:55:44 +0600

    palindrome check using for loop

---
+++ Palindrome Checker/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] words = { "lol", "world", "racecar" ,"talented", "deified", "tent", "tenet" };
+
+foreach (string word in words)
+{
+    Console.WriteLine($"{word} is a palindrome: {IsPalindrome(word)}");
+}
+
+bool IsPalindrome(string word)
+{
+    for (int i = 0; i < word.Length / 2; i++)
+    {
+        if (word[i] != word[word.Length - i - 1])
+        {
+            return false;
+        }
+    }return true;
+}
